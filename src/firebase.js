e3d32db97eb9286062007f4ede8e0d4b1bed7175
Ptolemy <ptolemybarnes@gmail.com>
2015-09-26 16:32:45 +0100

    Renamed function on firebase wrapper to 'row'

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ export default (function() {
   var FirebaseAPI = require("firebase");
   var firebase    = new FirebaseAPI("https://burning-inferno-9567.firebaseio.com/");
   
-  function getTweets(callback) {
+  function getData(callback) {
     firebase.child("tweets/").on("value", function(tweets) { 
       callback(tweets.val());
     })
   }
 
   return {
-    getTweets: getTweets
+    row: getData
   }
 })();
