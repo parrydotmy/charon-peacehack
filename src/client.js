5811be4a8afe8ece877d5a3bb13a72bd896e2d1f
Parry <parry@imin.uk.com>
2015-09-26 17:40:57 +0100

    Added a marker

--- src/client.js
+++ src/client.js
@@ -1,10 +1,34 @@
+import dataAPI from './firebase'
+import _ from 'underscore'
+
+window.dataAPI = dataAPI
+
 let map
 
-window.initMap = function () {
+function initMap() {
   map = new google.maps.Map(document.getElementById('map-mount'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
+  })
+}
+
+function renderMarker(dataPoint) {
+  let marker = new google.maps.Marker({
+    position: {lat: dataPoint.lat, lng: dataPoint.long},
+    map: map,
+    title: dataPoint.text
   });
 }
 
-$( document ).ready(window.initMap)
+function renderData(dataList) {
+  _.each(dataList, renderMarker)
+}
+
+function initialise() {
+  initMap()
+  // Grab data
+  dataAPI.row(renderData)
+  // Add markers to map
+}
+
+$( document ).ready(initialise)
