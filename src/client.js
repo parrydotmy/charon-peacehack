315f85ce14cc5650619f424c3524f846bfa310be
Parry <parry@imin.uk.com>
2015-09-26 19:22:53 +0100

    Refactor out map stuff

--- src/client.js
+++ src/client.js
@@ -1,38 +1,15 @@
 import dataAPI from './firebase'
 import _ from 'underscore'
 import infoDisplay from './infoDisplayer'
+import gmap from './gmap'
 
 window.dataAPI = dataAPI
 
 let map
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map-mount'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  })
-}
-
-function renderMarker(dataPoint) {
-  let marker = new google.maps.Marker({
-    position: {lat: dataPoint.lat, lng: dataPoint.long},
-    map: map,
-    title: dataPoint.text
-  })
-
-  marker.addListener('click', function() {
-    infoDisplay.display(dataPoint)
-  })
-}
-
-function renderData(dataList) {
-  _.each(dataList, renderMarker)
-}
-
 function initialise() {
-  initMap()
-  // Grab data, render it
-  dataAPI.row(renderData)
+  gmap.initialise()
+  dataAPI.row(gmap.renderData)
 }
 
 $( document ).ready(initialise)
