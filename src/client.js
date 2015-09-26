b94b673d5fad33a097dc779aa9976b8d227e1f85
Parry <parry@imin.uk.com>
2015-09-26 16:16:20 +0100

    Deleted dynamic files

--- src/client.js
+++ src/client.js
@@ -1,3 +1,7 @@
-import Firebase from 'firebase'
-
-console.log('test')
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map-mount'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
